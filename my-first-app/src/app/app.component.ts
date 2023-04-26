7f43973ac98d129fb7e4690a3f14d06556e3a83b
ksantos16 <u121364866@byui.edu>
2023-04-26 00:03:31 -0400

    angular basics

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // styles: [`
+  // h3{
+  // color: dodgerblue;
+  // }
+  //`]
 })
 export class AppComponent {
+  username = '';
+  showSecret = false;
+  log = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1)
+  }
+
 }
