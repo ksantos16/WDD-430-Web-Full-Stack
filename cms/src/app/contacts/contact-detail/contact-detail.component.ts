4be48237936216754b02226eedb348a9bf59ec2a
ksantos16 <u121364866@byui.edu>
2023-04-28 21:29:13 -0400

    aligning contact list and added OnInit to detail.t

--- cms/src/app/contacts/contact-detail/contact-detail.component.ts
+++ cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,28 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Contact } from '../contact.model';
 
 @Component({
-  selector: 'app-contact-detail',
+  selector: 'cms-contact-detail',
   templateUrl: './contact-detail.component.html',
   styleUrls: ['./contact-detail.component.css']
 })
-export class ContactDetailComponent {
-
-  contacts: Contact[] = [
+export class ContactDetailComponent implements OnInit {
+  contacts: Contact  =
     new Contact(
       "1",
       "R. Kent Jackson",
       "jacksonk@byui.edu",
       "208-496-3771",
       "../../../assets/images/jacksonk.jpg",
-      null),
-    new Contact(
-        "2",
-        "Rex Barzee",
-        "barzeer@byui.edu",
-        "208-496-3768",
-        "../../../assets/images/barzeer.jpg",
-        null,)
-  ];
+      null)
+  ;
+  constructor() { }
+
+  ngOnInit() {
+    // Called after the constructor and called  after the first ngOnChanges()
+ }
 
 }
