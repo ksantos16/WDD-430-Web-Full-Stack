f0afe9e56b0a69d7875ba980a3f1b5c27a78897e
ksantos16 <u121364866@byui.edu>
2023-05-10 22:14:11 -0400

    document components built

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
   templateUrl: './document-list.component.html',
   styleUrls: ['./document-list.component.css']
 })
-export class DocumentListComponent {
+export class DocumentListComponent implements OnInit{
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  documents: Document[] = [
+    new Document(
+      '1',
+      'Document 1',
+      'Document 1 Description',
+      'link 1',
+      null),
+    new Document(
+      '2',
+      'Document 2',
+      'Document 2 Description',
+      'link 2',
+      null),
+    new Document(
+      '3',
+      'Document 3',
+      'Document 3 Description',
+      'link 3',
+      null),
+    new Document(
+      '4',
+      'Document 4',
+      'Document 4 Description',
+      'link 4',
+      null),
+  ];
+
+  constructor() { }
+
+  ngOnInit(){
+  }
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 
 }
