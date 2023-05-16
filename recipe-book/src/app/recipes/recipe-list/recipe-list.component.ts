dd35c6f0394065c2c617aacb188392e0cd51a99d
ksantos16 <u121364866@byui.edu>
2023-05-15 23:04:58 -0400

    recipe services

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,26 +8,17 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit{
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test',
-    'https://2.bp.blogspot.com/-1j8L6E-iRxo/WABoajHGrZI/AAAAAAAAFso/JhWbpN3O2HooD8wlQDhllVhzo8VAu4AtgCLcB/s1600/20161011_120527.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply a test',
-    'https://2.bp.blogspot.com/-1j8L6E-iRxo/WABoajHGrZI/AAAAAAAAFso/JhWbpN3O2HooD8wlQDhllVhzo8VAu4AtgCLcB/s1600/20161011_120527.jpg'),
 
+  recipes: Recipe[];
 
-  ];
-
-  constructor() {
+  constructor(private recipeService: RecipeService) {
 
   }
 
   ngOnInit() {
-
+    this.recipes = this.recipeService.getRecipes();
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
+
 
 }
---
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -0,0 +1,35 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Recipe } from "./recipe-list/recipe.model";
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test',
+    'https://2.bp.blogspot.com/-1j8L6E-iRxo/WABoajHGrZI/AAAAAAAAFso/JhWbpN3O2HooD8wlQDhllVhzo8VAu4AtgCLcB/s1600/20161011_120527.jpg',
+    [
+      new Ingredient('Meat', 1),
+      new Ingredient('French Fries', 20)
+    ]),
+    new Recipe('Another Test Recipe', 'This is simply a test',
+    'https://2.bp.blogspot.com/-1j8L6E-iRxo/WABoajHGrZI/AAAAAAAAFso/JhWbpN3O2HooD8wlQDhllVhzo8VAu4AtgCLcB/s1600/20161011_120527.jpg',
+    [
+      new Ingredient('Buns', 2),
+      new Ingredient('Meat', 1)
+    ]),
+  ];
+
+  constructor (private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+
+}
